3bc87646f640fd5b6d7aaa6958ad71123ec52c44
Antonio Sabatino <antonio.sabatino@soprasteria.com>
2023-02-10 13:22:43 +0100

    skeleton on parent lwc created

--- force-app/main/default/lwc/ssgProductSelector/ssgProductSelector.js
+++ force-app/main/default/lwc/ssgProductSelector/ssgProductSelector.js
@@ -5,17 +5,27 @@
  * @last modified on  : 02-10-2023
  * @last modified by  : Antonio Sabatino
 **/
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 import DESKTOP_TEMPLATE from './desktopTemplate';
 import PHONE_TEMPLATE from './phoneTemplate';
 
 export default class SsgProductSelector extends LightningElement {
 
+	_objectSettings = {};
+	_isFirstPage = false;
+
+	@track status;
+
 	/* GETTERS AND SETTERS */
 	get isDesktop() {return FORM_FACTOR === 'Large' || FORM_FACTOR === 'Medium' ? true : false;}
+	get isSelecting() {return this._isFirstPage;}
 
 	/* STANDARD FUNCTIONS */
 	render(){return this.isDesktop ? DESKTOP_TEMPLATE : PHONE_TEMPLATE;}
+
+	connectedCallback(){
+		this._isFirstPage = true;
+	}
 
 }
